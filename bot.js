fd84857844457fa687b9b59c52d93b67a49ed811
chaque1111 <minemax378@gmail.com>
2024-10-08 14:29:58 -0300

    feat: conectando teams Direct Line con el chatbot de embeddings

---
+++ bot.js
@@ -0,0 +1,53 @@
+const {ActivityHandler, MessageFactory} = require("botbuilder");
+
+class EchoBot extends ActivityHandler {
+  constructor() {
+    super();
+    this.onMessage(async (context, next) => {
+      const request = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          text: context.activity.text,
+        }),
+      };
+      const response = await fetch(process.env.ChatBotUrl, request);
+
+      if (!response.ok) {
+        console.error(`Error en la respuesta: ${response.statusText}`);
+        await context.sendActivity(
+          MessageFactory.text(
+            "Hubo un problema con el servicio. Inténtalo más tarde.",
+            "error"
+          )
+        );
+        return;
+      }
+      const responseData = await response.json();
+      const responseText = responseData.text;
+      await context.sendActivity(
+        MessageFactory.text(responseText, responseText)
+      );
+
+      await next();
+    });
+
+    // this.onMembersAdded(async (context, next) => {
+    //     const membersAdded = context.activity.membersAdded;
+    //     const welcomeText = "Hello and welcome!";
+    //     for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
+    //         if (membersAdded[cnt].id !== context.activity.recipient.id) {
+    //             await context.sendActivity(
+    //                 MessageFactory.text(welcomeText, welcomeText)
+    //             );
+    //         }
+    //     }
+    //     // By calling next() you ensure that the next BotHandler is run.
+    //     await next();
+    // });
+  }
+}
+
+module.exports.EchoBot = EchoBot;
---
+++ index.js
@@ -0,0 +1,72 @@
+const path = require("path");
+
+const dotenv = require("dotenv");
+const ENV_FILE = path.join(__dirname, ".env");
+dotenv.config({path: ENV_FILE});
+
+const restify = require("restify");
+
+// Import required bot services.
+// See https://aka.ms/bot-services to learn more about the different parts of a bot.
+const {
+  CloudAdapter,
+  ConfigurationBotFrameworkAuthentication,
+} = require("botbuilder");
+
+const {EchoBot} = require("./bot");
+
+// Create HTTP server
+const server = restify.createServer();
+server.use(restify.plugins.bodyParser());
+
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+  console.log(`\n${server.name} listening to ${server.url}`);
+  console.log(
+    "\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator"
+  );
+  console.log('\nTo talk to your bot, open the emulator select "Open Bot"');
+});
+
+const botFrameworkAuthentication = new ConfigurationBotFrameworkAuthentication(
+  process.env
+);
+
+// Create adapter.
+// See https://aka.ms/about-bot-adapter to learn more about how bots work.
+const adapter = new CloudAdapter(botFrameworkAuthentication);
+
+// Catch-all for errors.
+const onTurnErrorHandler = async (context, error) => {
+  console.error(`\n [onTurnError] unhandled error: ${error}`);
+  await context.sendTraceActivity(
+    "OnTurnError Trace",
+    `${error}`,
+    "https://www.botframework.com/schemas/error",
+    "TurnError"
+  );
+
+  await context.sendActivity("The bot encountered an error or bug.");
+  await context.sendActivity(
+    "To continue to run this bot, please fix the bot source code."
+  );
+};
+
+// Set the onTurnError for the singleton CloudAdapter.
+adapter.onTurnError = onTurnErrorHandler;
+
+// Create the main dialog.
+const myBot = new EchoBot();
+
+// Listen for incoming requests.
+server.post("/api/messages", async (req, res) => {
+  await adapter.process(req, res, (context) => myBot.run(context));
+});
+
+server.on("upgrade", async (req, socket, head) => {
+  const streamingAdapter = new CloudAdapter(botFrameworkAuthentication);
+  streamingAdapter.onTurnError = onTurnErrorHandler;
+
+  await streamingAdapter.process(req, socket, head, (context) =>
+    myBot.run(context)
+  );
+});
